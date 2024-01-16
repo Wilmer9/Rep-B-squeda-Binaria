9bd61cb1de9dd13898dce41214245c8dfdd7240d
Wilmer Alfredo Vela Quiso <wvelaqui@unjbg.edu.pe>
2024-01-16 12:31:21 -0500

    Tercer cambio

--- Prueba2.cpp
+++ Prueba2.cpp
@@ -8,8 +8,14 @@ int main(){
 	int izq,der;
 	int cent =0;
 	do{
-		
-		
+		m= (izq+der)/2;
+		if(A[m]==dato)
+		{
+			
+		}
+		else{
+			if(A[m]<dato]
+		}
 	}
 	while(izq <= der && cent ==1)
 	return 0;
