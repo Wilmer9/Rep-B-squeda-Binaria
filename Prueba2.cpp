6d82b6d4fc4764973614188c0a2bc92d9b9034bf
Wilmer Alfredo Vela Quiso <wvelaqui@unjbg.edu.pe>
2024-01-16 12:26:01 -0500

    SEGUNDO COM

---
+++ Prueba2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+	
+	int A, n;
+	
+	int izq,der;
+	int cent =0;
+	do{
+		
+		
+	}
+	while(izq <= der && cent ==1)
+	return 0;
+}
